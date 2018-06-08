adbd4c05903fa01a1a901d693aa1fae1b1af38f6
cosmiczilch <cosmiczilch@gmail.com>
2018-06-08 20:34:12 +0200

    Add TIMI_DEBUG to build flags. Add test scene to the build. Add Scratch.cs

---
+++ Assets/Scripts/Scratch/Scratch.cs
@@ -0,0 +1,31 @@
+﻿using TimiShared.Debug;
+using UnityEngine;
+
+public class Scratch : MonoBehaviour {
+
+    [SerializeField]
+    private Transform _blinkingCubeTransform;
+
+    [SerializeField]
+    private OVRCameraRig _cameraRig;
+
+	// Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+    void Update () {
+        bool isButtonDown = OVRInput.Get(OVRInput.Button.One) || OVRInput.Get(OVRInput.Button.Two);
+
+        if (this._blinkingCubeTransform != null) {
+            this._blinkingCubeTransform.gameObject.SetActive(!isButtonDown);
+        }
+
+        if (this._cameraRig != null) {
+            if (this._cameraRig.usePerEyeCameras == false && isButtonDown) {
+                TimiDebug.LogErrorColor("Switching", LogColor.red);
+            }
+            this._cameraRig.usePerEyeCameras = isButtonDown;
+        }
+    }
+}
