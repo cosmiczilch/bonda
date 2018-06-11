c4d520044c702a5fb9bd7aad586414430f571568
cosmiczilch <cosmiczilch@gmail.com>
2018-06-12 01:33:54 +0200

    Move star data models to Data/... Add AppDataModel and load it in AppInit

---
+++ Assets/Scripts/Data/AppDataModel.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using ProtoBuf;
+using TimiShared.Debug;
+using TimiShared.Service;
+using TimiShared.Utils;
+
+public class AppDataModel : IService {
+
+    public static AppDataModel Instance {
+        get {
+            return ServiceLocator.Service<AppDataModel>();
+        }
+    }
+
+    #region Data
+    private StarsData _starsData;
+    public StarsData Stars {
+        get {
+            return this._starsData;
+        }
+    }
+    #endregion
+
+    public void LoadData(string filePath) {
+        FileStream fileStream = FileUtils.OpenFileStream(filePath, FileMode.Open, FileAccess.Read);
+        if (fileStream != null) {
+            this._starsData = Serializer.Deserialize<StarsData>(fileStream);
+            FileUtils.CloseFileStream(fileStream);
+        }
+
+        TimiDebug.LogColor("AppDataModel loaded ", LogColor.green);
+        TimiDebug.LogColor("Loaded " + this.Stars.stars.Count + " stars", LogColor.green);
+    }
+}
--- Assets/Scripts/Init/AppInit.cs
+++ Assets/Scripts/Init/AppInit.cs
@@ -1,4 +1,5 @@
 using TimiShared.Init;
+using TimiShared.Service;
 using UnityEngine;
 
 public class AppInit : MonoBehaviour, IInitializable {
@@ -7,8 +8,16 @@ public class AppInit : MonoBehaviour, IInitializable {
     public static System.Action OnAppInitComplete = delegate {};
 #endregion
 
+    // TODO: Move this somewhere else. This is too janky
+    [SerializeField]
+    private string _starsDataPath = "Assets/Resources/Data/starsData";
+
 #region IInitializable
     public void StartInitialize() {
+        AppDataModel appDataModel = new AppDataModel();
+        appDataModel.LoadData(this._starsDataPath);
+        ServiceLocator.RegisterService<AppDataModel>(appDataModel);
+
         this.IsFullyInitialized = true;
         OnAppInitComplete.Invoke();
     }
