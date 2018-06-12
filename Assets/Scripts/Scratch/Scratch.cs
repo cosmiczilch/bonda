57b9c80823c97f31e0e61829d81af9d65b27cff1
cosmiczilch <cosmiczilch@gmail.com>
2018-06-13 01:04:20 +0200

    Mark AppInit and AppLoader to run serially.

--- Assets/Scripts/Scratch/Scratch.cs
+++ Assets/Scripts/Scratch/Scratch.cs
@@ -4,16 +4,7 @@ using UnityEngine;
 public class Scratch : MonoBehaviour {
 
 	// Use this for initialization
-    void Start () {
-        AppInit.OnAppInitComplete += this.OnAppInitComplete;
-        this.OnAppInitComplete();
-    }
-
-    private void OnDestroy() {
-        AppInit.OnAppInitComplete -= this.OnAppInitComplete;
-	}
-
-	private void OnAppInitComplete() {
+    private void Start () {
         if (AppDataModel.Instance.StarsData == null || AppDataModel.Instance.StarsData.stars == null) {
             TimiDebug.LogErrorColor("No stars loaded", LogColor.red);
         }
